d83800a74d24532c0044097929be5b702df88636
Michael Pope <popemray1986@gmail.com>
2018-10-21 20:13:14 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories?client_id=884ad172428f11b9eca7&client_secret=21feb4ffedaef0e69b72dbe5a8c30b907373704f&q=repo' do |req|
+      req.params['full_name']
   end
 end
